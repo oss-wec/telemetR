4c74ec7e4bc87c5a31ef28b1cbd25806c1b437c6
kissmygritts <kissmygritts08@gmail.com>
2017-05-16 09:50:15 -0700

    server data source

--- server.R
+++ server.R
@@ -15,10 +15,10 @@ library(magrittr)
 library(highcharter)
 source("global.R")
 
-dat <- read_csv("Collars.csv", n_max = 10000)
-dat_animal <- read_csv("Animals.csv")
-#dat <- read_csv('/home/ubuntu/data/collars.csv')
-#dat_animal <- read_csv('/home/ubuntu/data/animals.csv')
+#dat <- read_csv("Collars.csv", n_max = 10000)
+#dat_animal <- read_csv("Animals.csv")
+dat <- read_csv('/home/ubuntu/data/collars.csv', n_max = 10000)
+dat_animal <- read_csv('/home/ubuntu/data/animals.csv')
 
 shinyServer(function(input, output, session) {
 
